0343f9a848e2ef17f177f079acfbdbe70c710ca5
unknown <pjajith010@gmail.com>
2020-06-28 20:27:53 +0530

    errorText color

--- src/music_modules/errorText.js
+++ src/music_modules/errorText.js
@@ -2,8 +2,9 @@ const Discord = require('discord.js');
 module.exports =(message,errorText) => {
     const embed = new Discord.MessageEmbed()
     .setTitle('Music :musical_note:')
-	    .addFields(
+    .setColor('0xFF1A00')
+	.addFields(
             { name: 'ERROR!', value: errorText }
-        );
+    );
     message.reply(embed);
 } 
